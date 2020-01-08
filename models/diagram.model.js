5809c0ff4a68dd6c8f149302f94d47d6268b167c
Andrada Denisa <Andradaz>
2020-01-08 10:41:12 +0200

    Added index to diagram model

--- models/diagram.model.js
+++ models/diagram.model.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema
 
 let DiagramSchema = new Schema({
     name: {type: String, required: true, max: 40},
-    link: {type: String, required: true, max:3}
+    link: {type: String, required: true, max:3},
+    index: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('Diagram', DiagramSchema)
